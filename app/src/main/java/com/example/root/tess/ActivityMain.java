7c57775de40a365a6c90b9f3245d2d7c85bec0ac
konstantin <indianor201203@gmail.com>
2017-03-25 02:11:42 +0500

    HelloWorld

---
+++ app/src/main/java/com/example/root/tess/ActivityMain.java
@@ -0,0 +1,16 @@
+package com.example.root.tess;
+
+import android.graphics.Bitmap;
+
+public class ActivityMain {
+    public static void main(String[] args) {
+
+    }
+
+    private class TessApi {
+        private String BitmapToText(Bitmap bitmap) {
+            return "";
+        }
+    }
+
+}
